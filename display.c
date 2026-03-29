9ed7ec6fea70ab3ab4fca0fb10b6f4ef83bee4fb
RaihanESP <raihan.elshirazy.tif25@polban.ac.id>
2026-03-29 18:20:44 +0700

    ANSI

--- display.c
+++ display.c
@@ -1,3 +1,22 @@
-﻿//
-// Created by ADVAN on 10/03/2026.
-//
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#include "display.h"
+#include "buffer.h"
+
+
+
+switch (key) {
+    case KEY_UP:;break;
+    case KEY_DOWN:;break;
+    case KEY_LEFT:;break;
+    case KEY_RIGHT:;break;
+    case KEY_QUIT:
+
+        exit(0);
+        break;
+}
