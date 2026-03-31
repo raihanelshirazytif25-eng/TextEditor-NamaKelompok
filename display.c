c71b2261212403970223985955b2e7e24fd83bf4
Kevin <kevin.akbar.tif25@polban.ac.id>
2026-03-31 23:01:20 +0700

    test makefile/project

--- display.c
+++ display.c
@@ -1,22 +1,21 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <termios.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
+#include <conio.h>
+#include <windows.h>
 
 #include "display.h"
 #include "buffer.h"
 
 
 
-switch (key) {
-    case KEY_UP:;break;
-    case KEY_DOWN:;break;
-    case KEY_LEFT:;break;
-    case KEY_RIGHT:;break;
-    case KEY_QUIT:
-
-        exit(0);
-        break;
-}
+//switch (key) {
+//    case KEY_UP:;break;
+//    case KEY_DOWN:;break;
+//    case KEY_LEFT:;break;
+//    case KEY_RIGHT:;break;
+//    case KEY_QUIT:
+//
+//        exit(0);
+//        break;
+//}
--- main.c
+++ main.c
@@ -1,7 +1,11 @@
-﻿#include "display.h"
+#include <stdio.h>
+
+
+#include "display.h"
 #include "buffer.h"
 #include "fileio.h"
 
 int main() {
+	printf("I nate higgerS");
     return 0;
 }
