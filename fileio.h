d3d918eae988d780f1ced8c8a5adf57463433fe4
DaoaAup <daffa.rauf.tif25@polban.ac.id>
2026-04-04 05:09:32 +0700

    adding function header into fileio.h

--- fileio.h
+++ fileio.h
@@ -1,8 +1,23 @@
-﻿//
-// Created by ADVAN on 10/03/2026.
-//
-
-#ifndef TEXTEDITOR_NAMAKELOMPOK_FILEIO_H
+﻿#ifndef TEXTEDITOR_NAMAKELOMPOK_FILEIO_H
 #define TEXTEDITOR_NAMAKELOMPOK_FILEIO_H
+
+#include <stdio.h>
+
+#ifndef MAX_COLS
+#define MAX_COLS 1024
+#endif
+
+extern char **buffer2D;
+extern int max_lines;
+extern int num_lines;
+extern int cx;
+extern int cy;
+extern int row_offset;
+extern char current_filename[256];
+
+void saveFile(void);
+void saveAsFile(void);
+void openFile(void);
+void exitManager(void);
 
 #endif //TEXTEDITOR_NAMAKELOMPOK_FILEIO_H
