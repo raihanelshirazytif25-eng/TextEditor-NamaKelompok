eb3b6348fb9c04fb3c101f489fc43e02f9c7ac6d
DaoaAup <daffa.rauf.tif25@polban.ac.id>
2026-04-07 15:02:25 +0700

    deleting anomaly at the first line and cleaning fileio .h and .c

--- fileio.h
+++ fileio.h
@@ -1,23 +1,9 @@
-﻿#ifndef TEXTEDITOR_NAMAKELOMPOK_FILEIO_H
-#define TEXTEDITOR_NAMAKELOMPOK_FILEIO_H
-
-#include <stdio.h>
-
-#ifndef MAX_COLS
-#define MAX_COLS 1024
-#endif
-
-extern char **buffer2D;
-extern int max_lines;
-extern int num_lines;
-extern int cx;
-extern int cy;
-extern int row_offset;
-extern char current_filename[256];
+#ifndef FILEIO_H
+#define FILEIO_H
 
 void saveFile(void);
 void saveAsFile(void);
 void openFile(void);
 void exitManager(void);
 
-#endif //TEXTEDITOR_NAMAKELOMPOK_FILEIO_H
+#endif
