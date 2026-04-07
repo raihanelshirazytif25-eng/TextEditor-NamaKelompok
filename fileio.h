be25dc34b527697d938e5e5053d168708903aa0d
DaoaAup <daffa.rauf.tif25@polban.ac.id>
2026-04-07 23:16:17 +0700

    Massive fix for openFile Function, saveFile, and changing malloc-based storage to 2D array

--- fileio.h
+++ fileio.h
@@ -6,5 +6,7 @@ int  flattenBuffer(char **out, int *outLen);
 void parseToBuffer(const char *raw, int rawLen);
 void openFile(void);
 void saveFile(void);
+void autoSave(void);
+void exitManager(void);
 
 #endif //TEXTEDITOR_NAMAKELOMPOK_FILEIO_H
